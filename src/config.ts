64947ff8bf7ed44c09d39831a1130eb73f94a5c5
LI Donghao <64763382+Kafka3@users.noreply.github.com>
2022-12-11 14:21:01 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SITE = {
   author: 'donghao',
   description: '记录📝',
   keywords: 'books,music,song,study,hk',
-  icon: 'https://s1.xptou.com/2022/12/11/639561c33276a.png',
+  icon: 'https://s1.xptou.com/2022/12/11/6395761066eb9.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
   homePage: 'https://weekly.tw93.fun',
   blogPage: 'https://tw93.fun/',
