bb3023c429b504cb7bda9bb855c238f0a0fcf440
LI Donghao <64763382+Kafka3@users.noreply.github.com>
2022-12-11 15:08:48 +0800

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -14,7 +14,7 @@ export const get = () =>
     title: '乱翻书',
     description: '记录每周',
     site: 'https://www.dongh.tech/',
-    customData: `<image><url>https://s1.xptou.com/2022/12/11/63957bfc185db.png</url></image>`,
+    customData: `<image><url>https://s1.xptou.com/2022/12/11/639581b92baca.png</url></image>`,
     items: posts.map((item) => {
       const url = item.url;
       const oldTitle = url.split('/posts/')[1];
